1b59242f95bef40cf174e960c444e36f28feab2b
BryceJohnson9 <bjohnson@stcharlessd.org>
2020-01-30 13:31:39 -0600

    modified:   Game library.py 	new file:   game_lib.pickle

--- Game library.py
+++ Game library.py
@@ -1,20 +1,95 @@
 #!/usr/bin/python3
 #Bryce Johnson
-#1-27-20
+#1-27-20    
 
 import pickle
-
+games = {1:["FPS", "Halo 3", "Bungee", "Microsoft", "Xbox 360", "2007", "10", "either", "30.00", "yes", "1/15/2008", "This game blows chunks"], 2:["Fighting", "Mortal Combat 11", "Netherealm Studios", "Warner Bros. Ineractive Entertainment", "Xbox One", "2019", "82%", "Both", "39.99", "No", "4/23/19", "You use combos to KO opponents"]}
 ''' Gives the user different options to pick games '''
 
 def add_game():
     print ("Running add_game()")
-    #game = input("What is the game you would like to add?")
-    #if game in base:
-        #print(" GAME ALREADY EXISTS. ")
-    #else:
-def print_all():
-    print ("Running print_all()")
+    new_key = len(games) + 1
+    new_entry = []
+    valid = False
+    while not valid:
+        genre = input("What is the genre? ")
+        new_entry.append(genre)
+        title = input("What is the tile? ")
+        new_entry.append(games)
+        developer = input("What company developed the game? ")
+        new_entry.append(developer)
+        publisher = input("What company published the game? ")
+        new_entry.append(publisher)
+        system = input("What system is the game for? ")
+        new_entry.append(system)
+        release_date = input("When was the game released? ")
+        new_entry.append(release_date)
+        rating = input("What is the rating of the game? ")
+        new_entry.append(rating)
+        multi_single_both = input("Is the game single playe, multiplaer, or both? ")
+        new_entry.append(multi_single_both)
+        price = input("What is the price of the game? ")
+        new_entry.append(price)
+        completion = input("Did you complete the game? ")
+        new_entry.append(completion)
+        purchase_date = input("When is the date you purchased the game? ")
+        new_entry.append(purchase_date)
+        notes = input("What is the description of the game? ")
+        new_entry.append(notes)
+        answer = input("Is this correct?")
+        if answer.lower() in ("yes" or "y"):
+            valid = True
+        
+    games[new_key] = new_entry
     
+def edit_game():
+    print("Here is the Library: ")
+    edit_entry = games[edit]
+    for key in games.keys():
+        print(key, "-", games[key][1])
+    edit_key = int(input("Which game do you want to change?"))
+    print("Current genre:", edit_entry[0])
+    edit_entry[0] = input("New genre:")
+    print("Current Title:" , edit_entry[1])
+    edit_entry[1] = input("New Title:")
+    print("Current developer:", edit_entry[2])
+    edit_entry[2] = input("New developer:")
+    print("Current publisher:", edit_entry[3])
+    edit_entry[3] = input("New publisher:")
+    print("Current system:", edit_entry[4])
+    edit_entry[4] = input("New system:")
+    print("Current release date:", edit_entry[5])
+    edit_entry[5] = input("New release date:")
+    print("Current rating:", edit_entry[6])
+    edit_entry[6] = input("New rating:")
+    print("Current Multiplayer:", edit_entry[7])
+    edit_entry[7] = input("New multiplayer:")
+    print("Current price:", edit_entry[8])
+    edit_entry[8] = input("New price:")
+    print("Current completion:", edit_entry[9])
+    edit_entry[9] = input("New completion:")
+    print("Current Purchase date:", edit_entry[10])
+    edit_entry[10] = input("New purchase date:")
+    print("Current notes:", edit_entry[11])
+    edit_entry[11] = input("New notes:")
+                   
+def print_all():
+    key_list = games.keys()
+    for key in key_list:
+        print()
+        print("Genre: ", games[key][0])
+        print("Title: ", games[key][1])
+        print("Developer: ", games[key][2])
+        print("Publisher:", games[key][3])
+        print("System: ", games[key][4])
+        print("Release date: ", games[key][5])
+        print("Rating: ", games[key][6])
+        print("Multi/single/both: ", games[key][7])
+        print("Price: ", games[key][8])
+        print("Beat it?: ", games[key][9])
+        print("Purchase date: ", games[key][10])
+        print("Notes: ", games[key][11])
+        
 def search_title():
     print ("Running search_title()")
     
@@ -24,10 +99,34 @@ def remove_game():
 def save_data():
     print ("Running save_data()")
     
+def search():
+    print ("Running search()")
+    genre = input("What is the game you would like to search?")
+    for key in games.keys():
+        print(games[key][0])
+        if genre in games[key][0]:
+            print()
+            print("Genre: ", games[key][0])
+            print("Title: ", games[key][1])
+            print("Developer: ", games[key][2])
+            print("Publisher: ", games[key][3])
+            print("System: ", games[key][4])
+            print("Release date: ", games[key][5])
+            print("Rating: ", games[key][6])
+            print("Multi/single/both: ", games[key][7])
+            print("Price: ", games[key][8])
+            print("Beat it?: ", games[key][9])
+            print("Purchase date: ", games[key][10])
+            print("Notes: ", games[key][11])
+    
 def quit():
     print("Running quit()")
     
 keep_going = True
+
+data_file = open("game_lib.pickle", "rb")
+games = pickle.load(data_file)
+data_file.close()    
 
 while keep_going:
     print("""
@@ -35,11 +134,13 @@ while keep_going:
     ---------------------------
     
     MAIN MENU
-    1) Add/Edit game
-    2) Print all games
-    3) Search by title    
-    4) Remove a game
-    5) Save database
+    1) Add game
+    2) Edit game
+    3) Print all games
+    4) Search by title    
+    5) Remove a game
+    6) Save database
+    7) Search
     
     Q) Quit
     
@@ -49,13 +150,17 @@ while keep_going:
     if choice == "1":
         add_game()
     elif choice == "2":
-        print_all()
+        edit_game()
     elif choice == "3":
-        search_title()       
+        print_all()       
     elif choice == "4":
-        remove_game(True)
+        search_title(True)
     elif choice == "5":
+        remove_game()
+    elif choice == "6":
         save_data()
+    elif choice == "7":
+        search()
     elif choice == "Q":
         quit()
         
